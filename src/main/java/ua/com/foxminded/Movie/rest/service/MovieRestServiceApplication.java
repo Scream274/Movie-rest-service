6f86c6ef457b5ca3589d1b1e58ba2ecb42cb75a8
Anatolii <omelchenko20@gmail.com>
2023-05-30 17:13:20 +0300

    Initial commit

---
+++ src/main/java/ua/com/foxminded/Movie/rest/service/MovieRestServiceApplication.java
@@ -0,0 +1,13 @@
+package ua.com.foxminded.Movie.rest.service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MovieRestServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MovieRestServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ua/com/foxminded/Movie/rest/service/MovieRestServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package ua.com.foxminded.Movie.rest.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieRestServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
